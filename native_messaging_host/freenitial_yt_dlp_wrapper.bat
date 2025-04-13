4eb5d7122b137c3dec11fa89278d36f3e8f8a9d8
Léo Gillet <83383164+Freenitial@users.noreply.github.com>
2025-04-14 01:26:32 +0200

    Update freenitial_yt_dlp_wrapper.bat

--- native_messaging_host/freenitial_yt_dlp_wrapper.bat
+++ native_messaging_host/freenitial_yt_dlp_wrapper.bat
@@ -1,2 +1,2 @@
 @echo off & cd /d %~dp0
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "freenitial_yt_dlp_script.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "freenitial_yt_dlp_script.ps1"
